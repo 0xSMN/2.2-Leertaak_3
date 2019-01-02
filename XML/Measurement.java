6c3a0547900705f3ddb8c3b0a4c5e4426fdfa192
@ErrorCode51 <43345236+ErrorCode51@users.noreply.github.com>
2019-01-02 20:35:27 +0100

    Update Measurement.java

--- XML/Measurement.java
+++ XML/Measurement.java
@@ -1,9 +1,20 @@
 package XML;
 
 import java.util.ArrayList;
+import java.util.Date;
 
 public class Measurement {
-    public String name;
-    public String Content;
-    public ArrayList childeren = new ArrayList<Measurement>(); // a list for the childeren of the object
+    public int STN;
+    public Date DATETIME;
+    public float TEMP;
+    public float DEWP;
+    public float STP;
+    public float SLP;
+    public float VISIB;
+    public float WDSP;
+    public float PRCP;
+    public float SNDP;
+    public byte FRSHTT;
+    public float CLDC;
+    public short WNDDIR;
 }
