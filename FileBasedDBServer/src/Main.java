4a57350f7a36e1d89d266239a53bd73f268af363
danielgeerts7 <daniel.geerts@live.nl>
2019-01-09 15:01:36 +0100

    Initialize File Based Server

---
+++ FileBasedDBServer/src/Main.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // write your code here
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("What is your name: ");
+        String username = scanner.next();
+        System.out.println("Welkom " + username);
+    }
+}
