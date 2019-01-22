f906c706795a4577bcaf9913717c9b898282efcc
annemarijke99 <a.m.dusseljee@st.hanze.nl>
2019-01-23 00:21:48 +0100

    fixed login page error

--- webapp/index.php
+++ webapp/index.php
@@ -5,6 +5,7 @@ include('header.php');
 
 if ($loggedin) {
 	include('sidebar.php');
+	include('welcome.php');
 } else {
 	include('login.php');
 }
@@ -12,16 +13,3 @@ if ($loggedin) {
 include('footer.php');
 
 ?>
-
-<html>
-<head>
-</head>
-<body>
-
-<p class="title">Welcome</p>
-
-<p class="text">This is the weather application for UIC Crop Insurance Pakistan.</p>
-<p class="text">On the page "Rainfall" you can view a live data visualisation of the rainfall in all the locations with weather stations in Pakistan.</p>
-<p class="text">You can also look up weather data up to four weeks back on the page "Weather History".</p>
-
-</body>
---
+++ webapp/welcome.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: AnneMarijke
+ * Date: 23-1-2019
+ * Time: 00:17
+ */
+?>
+
+<html>
+<head>
+</head>
+<body>
+<br>
+<p class="title">Welcome</p>
+
+<p class="text">This is the weather application for UIC Crop Insurance Pakistan.</p>
+<p class="text">On the page "Rainfall" you can view a live data visualisation of the rainfall in all the locations with weather stations in Pakistan.</p>
+<p class="text">You can also look up weather data up to four weeks back on the page "Weather History".</p>
+
+</body>
+</html>
