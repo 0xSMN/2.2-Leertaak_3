3f529aa09e7468ea6f8f00816519505c0d4035b1
ErrorCode51 <43345236+errorcode51@users.noreply.github.com>
2019-01-22 13:01:06 +0100

    F*** Deze

--- WeatherApp/Server/Server.java
+++ WeatherApp/Server/Server.java
@@ -40,20 +40,7 @@ public class Server {
                 // a PrintWriter object to write the reply to the client to
                 PrintWriter dataOut = new PrintWriter(connection.getOutputStream());
 
-
-                if (fileName.equals("/")) { // no specific filename given, using index.html
-                    fileName = "/index.html";
-                }
-
-                if (new File("HTML" + fileName).exists()) {
-                    // write the http-html header to the printWriter
-                    addHtmlHeader(dataOut);
-                    // add the html to the printWriter
-                    addHtmlByFilename(fileName, dataOut);
-                }
-                else {
-                    add404Page(dataOut);
-                }
+                getPageByFileName(fileName, dataOut);
 
                 // send everything and close the connection
                 dataOut.flush();
@@ -64,6 +51,56 @@ public class Server {
         ioe.printStackTrace();
         }
     }
+
+    private static void getPageByFileName(String URL, PrintWriter pw) {
+        String path = "HTML".concat(URL);
+        boolean success = false; // set to true if a page is successfully read
+        if (path.equals("HTML/")) { // no specific filename given, using index.html
+            path = "HTML/index.html";
+        }
+
+        success =  addStandardHtmlPage(path, pw);
+        if (!success) {
+            success = addStandardHtmlPage(path + ".html", pw);
+        }
+        if (!success) {
+            success = addStandardHtmlPage(path + "/index.html", pw);
+        }
+        if (!success) {
+            add404Page(pw);
+        }
+
+    }
+
+    private static boolean addStandardHtmlPage(String path, PrintWriter pw) {
+        System.out.println("File: " + path);
+
+        try {
+            // open the HTML file
+            BufferedReader fReader = new BufferedReader(new FileReader(path));
+            String outLine;
+
+            // write the http-html header to the printWriter
+            addHtmlHeader(pw);
+
+            // write the HTML to the printWriter
+            while ((outLine = fReader.readLine()) != null) {
+                if (outLine.equals("")) {
+                    continue;
+                }
+//                    System.err.println(outLine.trim());
+                pw.println(outLine.trim());
+            }
+        }
+        catch (IOException ex) {
+            System.err.println("Error: page could not be read");
+            pw.format("");
+            return false; // no file was found with the given name, or the file file could not be read
+        }
+
+        return true; // file was read successfully
+    }
+
 
     private static void addHtmlHeader(PrintWriter pw) {
         // write the http-html header to the printWriter
@@ -84,35 +121,14 @@ public class Server {
         return fileName;
     }
 
-    private static boolean addHtmlByFilename(String fileName, PrintWriter pw) {
-        try {
-            // open the HTML file
-            BufferedReader fReader = new BufferedReader(new FileReader("HTML" + fileName));
-            String outLine;
-
-            // write the HTML to the printWriter
-            while ((outLine = fReader.readLine()) != null) {
-                if (outLine.equals("")) {
-                    continue;
-                }
-//                    System.err.println(outLine.trim());
-                pw.println(outLine.trim());
-            }
-        }
-        catch (IOException ex) {
-            return false; // no file was found with the given name, or the file file could not be read
-        }
-
-        return true; // file was read successfully
-    }
-
     private static void add404Page(PrintWriter pw) {
+        System.err.println("404 Page");
         // write the http-html header to the printWriter
         pw.println("HTTP/1.1 404 Not Found");
         pw.println("Content-type:text/html");
         pw.println("Connection: close");
         pw.println(); // a blank line to indicate the end of the header
-        pw.print("<h1>404 - Not Found</h1>");
+        pw.println("<h1>404 - Not Found</h1>");
         pw.print("<p>Congratulations, you broke it</p>");
     }
 }
