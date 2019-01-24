f55d1f4ce40f39c248613df1ecfdff442518ffd7
annemarijke99 <a.m.dusseljee@st.hanze.nl>
2019-01-24 12:59:53 +0100

    Example graph

--- webapp/rainfall.php
+++ webapp/rainfall.php
@@ -54,13 +54,66 @@ include('footer.php');
         <br><br>
         <input type="submit" value="Submit" onclick="<?php $location = $_GET["location"];?>">
     </form>
-    <br><br><br>
-
-    <?php echo $location; ?>
-
 </div>
 
-<canvas class="canvas" id="raingraph" width="700" height="400"></canvas>
+    <?php
+
+    $dataPoints = array(
+        array("x" => 946665000000, "y" => 3),
+        array("x" => 978287400000, "y" => 3),
+        array("x" => 1009823400000, "y" => 2),
+        array("x" => 1041359400000, "y" => 2),
+        array("x" => 1072895400000, "y" => 2),
+        array("x" => 1104517800000, "y" => 1),
+        array("x" => 1136053800000, "y" => 1),
+        array("x" => 1167589800000, "y" => 2),
+        array("x" => 1199125800000, "y" => 1),
+        array("x" => 1230748200000, "y" => 2),
+        array("x" => 1262284200000, "y" => 6),
+        array("x" => 1293820200000, "y" => 5),
+        array("x" => 1325356200000, "y" => 4),
+        array("x" => 1356978600000, "y" => 3),
+        array("x" => 1388514600000, "y" => 2),
+        array("x" => 1420050600000, "y" => 1),
+        array("x" => 1451586600000, "y" => 2)
+    );
+
+    ?>
+        <script>
+            window.onload = function () {
+
+                var chart = new CanvasJS.Chart("chartContainer", {
+                    animationEnabled: true,
+                    title:{
+                        text: "<?php echo $location; ?>"
+                    },
+                    axisY: {
+                        title: "Rainfall in ml",
+                        suffix: "ml",
+                    },
+                    axisX: {
+                        title: "Minutes"
+                    },
+                    data: [{
+                        type: "spline",
+                        markerSize: 5,
+                        xValueFormatString: "DD-MM-YYYY",
+		                yValueFormatString: "#,#",
+		                xValueType: "dateTime",
+		                dataPoints: <?php echo json_encode($dataPoints, JSON_NUMERIC_CHECK); ?>
+                    }]
+                });
+
+                chart.render();
+
+            }
+        </script>
+
+    <div class="canvas" id="chartContainer" style="height: 400px; width: 700px;"></div>
+    <script src="https://canvasjs.com/assets/script/canvasjs.min.js"></script>
+
+
+</canvas>
 
 </body>
 
