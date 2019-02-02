26fae840012869cc84c613e89da03fcfcca691fe
annemarijke99 <a.m.dusseljee@st.hanze.nl>
2019-02-03 00:46:39 +0100

    change path to db for downloadxml

--- webapp/js/csv_to_xml.js
+++ webapp/js/csv_to_xml.js
@@ -58,7 +58,7 @@ $(document).ready(function() {
     const location = $('.location').text();
 
     //todo: change path when connected to db
-    let fileName = "http://localhost/2.2-Leertaak_3/webapp/_database_/"+date_split[0]+"/"+date_split[1]+"/"+date_split[2]+"/"+location+"/"+date+"_h"+hour+".csv";
+    let fileName = "/home/ITV2E02/Documents/_database_/"+date_split[0]+"/"+date_split[1]+"/"+date_split[2]+"/"+location+"/"+date+"_h"+hour+".csv";
     let csvfile = fileName.replace(/ /g, '');
 
 });
