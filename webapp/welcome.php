5a954443473eabc8ee4b956f3d33cd7f25eb321a
annemarijke99 <a.m.dusseljee@st.hanze.nl>
2019-02-04 12:57:30 +0100

    Added some text to the welcome page

--- webapp/welcome.php
+++ webapp/welcome.php
@@ -15,9 +15,11 @@
 <p class="title">Welcome</p>
 
 <p class="text">This is the weather application for UIC Crop Insurance Pakistan.</p>
-<p class="text">On the page "Rainfall" you can view a live data visualisation of the rainfall in all the locations with weather stations in Pakistan.</p>
+<p class="text">This application receives data from all the weather stations in Pakistan and neighboring countries.</p>
+<p class="text">On the page "Rainfall" you can view a live data visualisation of the rainfall in the location you select.</p>
 <p class="text">You can look up weather data up to four weeks back on the page "Weather History".</p>
 <p class="text">On the page "Download XML" you can download the weather data in XML format.</p>
+<p class="text">With flags on the pages you can choose another country to select stations from.</p>
 
 </body>
 </html>
